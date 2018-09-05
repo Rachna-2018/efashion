9634eb546ba5f4d34484e6b43d537548b6923cc6
Rachna-2018 <41289843+Rachna-2018@users.noreply.github.com>
2018-09-05 16:25:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -83,7 +83,7 @@ if($method == 'POST')
     		curl_setopt_array( $ch, $options );
 		$json = curl_exec( $ch );
 		$someobj = json_decode($json,true);
-		if($someobj[0]==null and $statecom == 'liststates')
+		if(empty($someobj) and $statecom == 'liststates')
 		{
 			$json_url = "http://74.201.240.43:8000/ChatBot/Sample_chatbot/EFASHION_DEV.xsjs?command=$statecom&STATE=$STATE&CITY=$CITY&SHOPNAME=$SHOPNAME&YR=$YR&QTR=$QTR&MTH=$MTH";		
 			//echo $json_url;
